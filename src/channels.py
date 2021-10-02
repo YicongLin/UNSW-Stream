54de589dec820a73f6449344e8ab932ced7d539d
Yicong Lin <z5368375@ad.unsw.edu.au>
2021-10-02 19:16:01 +1000

    update channel create function, append dictionaries to the datastore

--- src/channels.py
+++ src/channels.py
@@ -63,23 +63,49 @@ def channels_create_v1(auth_user_id, name, is_public):
     if (flag == 0):
         raise AccessError("Invalid ID")
     # find owner name
-    owner_name = users_info[0]['last_name']
-    for owner in users_info:
+    owner_first_name = users_info[auth_user_id - 1]['first_name']
+    owner_last_name = users_info[auth_user_id - 1]['last_name']
+    owner_email = users_info[auth_user_id - 1]['email']
+    handle_str = owner_first_name + owner_last_name
+    """ for owner in users_info:
         # since user id = counter - 1, for example 1st user's id is 1 and it is equal to users[0]['id']
+        if (users_info[owner]['first_name'] == users_info[auth_user_id - 1]['first_name']):
+            owner_first_name = users_info[owner]['first_name']
         if (users_info[owner]['last_name'] == users_info[auth_user_id - 1]['last_name']):
-            owner_name = users_info[owner]['last_name']
-    
+            owner_last_name = users_info[owner]['last_name']
+        if (users_info[owner]['email'] == users_info[auth_user_id - 1]['last_name']):
+ """
     # a dictionary for the channel
     channels_dict = {
         'channel_id': new_channel_id,
-        'channel_name': name,
-        'is_public': is_public,
-        'owner': owner_name,
-        'members': owner_name
+        'channel_name': name
     }
 
+    channels_detail_dict = {
+        'u_name': owner_first_name,
+        'is_public': is_public,
+        'owner_members': [
+            {
+                'u_id': auth_user_id,
+                'email': owner_email,
+                'name_first': owner_first_name,
+                'name_last': owner_last_name,
+                'handle_str': handle_str.lower()
+            }
+        ],
+        'all_members': [
+            {
+                'u_id': auth_user_id,
+                'email': owner_email,
+                'name_first': owner_first_name,
+                'name_last': owner_last_name,
+                'handle_str': handle_str.lower()
+            }
+        ]
+    }
     # append all data and return
     data['channels'].append(channels_dict)
+    data['channels_details'].append(channels_detail_dict)
     data_store.set(data)
     return {
         new_channel_id

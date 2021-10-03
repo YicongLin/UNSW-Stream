d0531e46c373191a4a90d35af7b4d41331c2dc50
Prasannaa <z5359919@ad.unsw.edu.au>
2021-10-03 18:09:50 +1100

    Adding channel_messages_v1

--- src/channel.py
+++ src/channel.py
@@ -26,6 +26,58 @@ def channel_details_v1(auth_user_id, channel_id):
     }
 
 def channel_messages_v1(auth_user_id, channel_id, start):
+    # Accessing the data store
+    data = data_store.get()
+    
+    # Creating a list of valid channel IDS
+    channels = data["channel_details"]
+    valid_channel_ids = []
+    for channel in channels:
+        valid_channel_ids.append(channel["channel_id"])
+    
+    # Error if channel ID is invalid 
+    if channel_id not in valid_channel_ids:
+        raise InputError("Invalid channel_id")
+    
+    # List of valid user IDS
+    users = data["users"]
+    valid_user_ids = []
+    for user in users:
+        valid_user_ids.append(user["user_id"])
+
+    # Error if the auth_user is not a member of the valid channel
+    members = channels[channel_count]["channel_members"]
+    member_of_channel = False
+    for member in members:
+        if auth_user_id == member["user_id"]:
+            member_of_channel = True
+    if member_of_channel == False:
+        raise AccessError("You are not a member of the channel")
+
+    # Function returns up to 50 messages
+    start = int(start)
+    max_messages = 50
+    messages_total = 0
+    
+    # Creating a messages dictionary
+    messages_dict = {message_id, u_id, message, time_created}:
+    messages_list = []
+    dict_copy = messages_dict.copy()
+    messages_list.append(dict_copy)
+    
+    # Check most recent message is 0 and the start is less than number of messages
+    if start >= messages_total and start != 0:
+        raise InputError("Start is greater than or equal to the total number of messages in the channel")
+    
+    # Index between start and start + 50
+    end = start + max_messages
+    if end > total_message:
+        # Indicating there are no more messages to load 
+        end = -1
+        
+return { messages, start, end }
+
+'''
     return {
         'messages': [
             {
@@ -38,6 +90,7 @@ def channel_messages_v1(auth_user_id, channel_id, start):
         'start': 0,
         'end': 50,
     }
+'''
 
 def channel_join_v1(auth_user_id, channel_id):
     return {

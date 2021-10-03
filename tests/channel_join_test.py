e52ea4406da7a0b4288ee0d52adb3d7efb6713f6
Prasannaa Pandey <p.pandey@student.unsw.edu.au>
2021-10-04 00:19:34 +1100

    Restoring to pre-commented out state

--- tests/channel_join_test.py
+++ tests/channel_join_test.py
@@ -5,8 +5,8 @@ from src.channels import channels_create_v1
 from src.auth import auth_register_v1
 from src.other import clear_v1
 
-Creating valid channel and user IDs, 
-with one public channel and one private channel
+# Creating valid channel and user IDs, 
+# with one public channel and one private channel
 @pytest.fixture
 def valid_2_users():
     clear_v1()
@@ -16,36 +16,36 @@ def valid_2_users():
     channel_id_2 = channels_create_v1(id_2, "zyx", False)
     return id_1, id_2, channel_id_1, channel_id_2
     
-Testing for invalid channel ID
+# Testing for invalid channel ID
 def test_invalid_channel(valid_2_users):
     id_1, *_ = valid_2_users
     with pytest.raises(InputError):
         channel_join_v1(id_1, "invalid_channel")
         
-Testing for a case where the user is already a member
+# Testing for a case where the user is already a member
 def test_already_a_member(valid_2_users):
     id_1, _, channel_id_1, _ = valid_2_users
     with pytest.raises(InputError):
         channel_join_v1(id_1, channel_id_1)
         
-Testing for valid channel ID, and user is not already a member
+# Testing for valid channel ID, and user is not already a member
 def test_valid(valid_2_users):
     _, id_2, channel_id_1, _ = valid_2_users
     channel_join_v1(id_2, channel_id_1)
 
-Testing for users attempting to join private channels
+# Testing for users attempting to join private channels
 def test_private_channel(valid_2_users):
     id_1, _, _, channel_id_2 = valid_2_users
     with pytest.raises(AccessError):
         channel_join_v1(id_1, channel_id_2)
 
-Testing for users attempting to join public channels
+# Testing for users attempting to join public channels
 def test_public_channel(valid_2_users):
     _, id_2, channel_id_1, _ = valid_2_users  
     channel_join_v1(id_2, channel_id_1)
 
 
-Testing cases for wrong input
+# Testing cases for wrong input
 def test_empty():
     with pytest.raises(InputError):
         channel_join_v1("", "")

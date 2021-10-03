e1bb0ec6462e10d60ecde3d346ecb65e616d4890
Lilian Pok <lilian.pok@gmail.com>
2021-10-03 23:14:18 +1100

    Fixing up an error

--- tests/channel_messages_test.py
+++ tests/channel_messages_test.py
@@ -11,27 +11,28 @@ from src.other import clear_v1
 def valid():
     clear_v1()
     id_1 = auth_register_v1("qwe@rty.com", "password", "uio", "qwe")
+    id_2 = auth_register_v1("asd@fgh.com", "password", "jkl", "asd")
     channel_id_1 = channels_create_v1(id_1, "qwe", True)
     channel_id_2 = channels_create_v1(id_2, "asd", False)
     start_5 = 5
     start_10 = 10
-    return id_1, channel_id_1, channel_id_2, start_5, start_10
+    return id_1, id_2, channel_id_1, channel_id_2, start_5, start_10
 
 # Testing for an invalid channel
 def test_invalid_channel(valid):
-    id_1, _, _, _, start_10 = valid
+    id_1, _, _, _, _, start_10 = valid
     with pytest.raises(InputError):
         channel_messages_v1(id_1, "invalid_channel", start_10)
         
 # Testing for a valid channel
 def test_valid_channel(valid):
-    id_1, channel_id_1, _, _, start_10 = valid
+    id_1, _, channel_id_1, _, _, start_10 = valid
     channel_messages_v1(id_1, channel_id_1, start_10)
     
 # Testing for when start is greater than the 
 # total number of messages in the channel
 def test_valid_channel(valid):
-    id_1, channel_id_1, _, _, start_10 = valid
+    id_1, _, channel_id_1, _, _, start_10 = valid
     data = data_store.get()
     messages = data["channels_details"]["messages"]
     assert len(messages) == 3
@@ -41,7 +42,7 @@ def test_valid_channel(valid):
 # Testing for when start is less than or equal to the 
 # total number of messages in the channel
 def test_valid_channel(valid):
-    id_1, channel_id_1, _, start_5, start_10 = valid
+    id_1, _, channel_id_1, _, start_5, start_10 = valid
     messages = range(0, 10)
     assert len(messages) == 10
     channel_messages_v1(id_1, channel_id_1, start_10)
@@ -50,14 +51,14 @@ def test_valid_channel(valid):
 # Testing for a case where the authorised user 
 # is not a member of the valid channel
 def test_not_a_member(valid):
-    id_1, _, channel_id_2, _, start_10 = valid
+    id_1, _, _, channel_id_2, _, start_10 = valid
     with pytest.raises(AccessError):
         channel_messages_v1(id_1, channel_id_2, start_10)
 
 # Testing for a case where the authorised member 
 # is a member of the valid channel
 def test_channel_member(valid):
-    id_1, channel_id_1, _, _, start_10 = valid
+    id_1, _, channel_id_1, _, _, start_10 = valid
     channel_messages_v1(id_1, channel_id_1, start_10)
 
 # Testing cases for other invalid input

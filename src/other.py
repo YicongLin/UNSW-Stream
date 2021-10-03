ff3b5cde87217d529e84754d9ace20ba39a0cf6e
Prasannaa Pandey <p.pandey@student.unsw.edu.au>
2021-10-03 20:54:29 +1100

    Removing functions not in data store

--- src/other.py
+++ src/other.py
@@ -4,11 +4,7 @@ def clear_v1():
     store = data_store.get()
     store['users'] = []
     store['channels'] = []
-    store['channel'] = []
     store['channels_details'] = []
-    store['channel_id'] = []
-    store['channel_status'] = []
-    store['members'] = []
 
     data_store.set(store)
    

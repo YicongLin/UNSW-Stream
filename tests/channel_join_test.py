6bcaa1ca49f8c6815c486e53178f50e0adeedd4b
Lilian Pok <lilian.pok@gmail.com>
2021-10-03 13:49:44 +1100

    Debugging

--- tests/channel_join_test.py
+++ tests/channel_join_test.py
@@ -7,57 +7,51 @@ from src.other import clear_v1
 
 # Creating valid channel and user IDs, 
 # with one public channel and one private channel
-def create_two_channels():
+def valid():
     clear_v1()
-    valid_id_1 = auth_register_v1("abc@abc.com", "password", "abc", "def")
-    valid_id_2 = auth_register_v1("zyx@wvu.com", "password", "zyx", "wvu")
-    valid_id_3 = auth_register_v1("mno@jkl.com", "password", "mno", "jkl")
-    valid_channel_id_1 = channels_create_v1(valid_id_1, "abc", True)
-    valid_channel_id_2 = channels_create_v1(valid_id_2, "zyx", False)
+    id_1 = auth_register_v1("abc@abc.com", "password", "abc", "def")
+    id_2 = auth_register_v1("zyx@wvu.com", "password", "zyx", "wvu")
+    channel_id_1 = channels_create_v1(id_1, "abc", True)
+    channel_id_2 = channels_create_v1(id_2, "zyx", False)
+    return id_1, id_2, channel_id_1, channel_id_2
     
 # Testing for invalid channel ID
 def test_invalid_channel():
-    create_two_channels()
+    id_1, *_ = valid()
     with pytest.raises(InputError):
-        channel_join_v1(valid_id_1, "invalid_channel")
+        channel_join_v1(id_1, "invalid_channel")
 
 # Testing for a case where the user is already a member
 def test_already_a_member():
-    create_two_channels()
+    id_1, _, channel_id_1, _ = valid()
     with pytest.raises(InputError):
-        channel_join_v1(valid_id_1, valid_channel_id_1)
-        channel_join_v1(valid_id_2, valid_channel_id_2)
+        channel_join_v1(id_1, channel_id_1)
 
 # Testing for users attempting to join private channels
 def test_private_channel():
-    create_two_channels()
+    id_1, _, _, channel_id_2 = valid()
     with pytest.raises(AccessError):
-        channel_join_v1(valid_id_1, valid_channel_2)
-        channel_join_v1(valid_id_3, valid_channel_2)
+        channel_join_v1(id_1, channel_id_2)
 
 # Testing cases for wrong input
 def test_empty():
-    create_two_channels()
-    with pytest.raises(AccessError):
+    with pytest.raises(InputError):
         channel_join_v1("", "")
 
 def test_symbols():
-    create_two_channels()
-    with pytest.raises(AccessError):
-        channel_join_v1("#&$_*%", "#$\(%}(")
+    with pytest.raises(InputError):
+        channel_join_v1("#&$_*%", "#$(%}(")
 
 def test_invalid_strings():
-    create_two_channels()
-    with pytest.raises(AccessError):
+    with pytest.raises(InputError):
         channel_join_v1("invalid_id", "invalid_channel")
 
 def test_combination():
-    create_two_channels()
-    with pytest.raises(AccessError):
+    with pytest.raises(InputError):
         channel_join_v1("", "invalid_channel")
         channel_join_v1("invalid_id", "")
-        channel_join_v1("", "#$\(%}(")
+        channel_join_v1("", "#$(%}(")
         channel_join_v1("#&$_*%", "")
         channel_join_v1("#&$_*%", "invalid_channel")
-        channel_join_v1("invalid_id", "#$\(%}(") 
+        channel_join_v1("invalid_id", "#$(%}(") 
 

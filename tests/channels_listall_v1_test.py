e708c5e57e8ff092698b110d55b81079ac93e551
z5346398 <z5346398@cse.unsw.edu.au>
2021-10-04 00:14:22 +1100

    Adjust tests

--- tests/channels_listall_v1_test.py
+++ tests/channels_listall_v1_test.py
@@ -14,24 +14,6 @@ def create_user():
     valid_id = [valid_id_1, valid_id_2, valid_id_3] # store valid user id for return multiple valid id
     return valid_id
 
-# @pytest.fixture
-# def channel_create(create_user):
-#     valid_id = create_user
-#     valid_id_1 = valid_id[0]
-#     valid_id_2 = valid_id[1]
-#     valid_id_3 = valid_id[2]
-#     channel_id1 = channels_create_v1(valid_id_1, 'Channel_one', True) # User with valid_id_1 create a Public channel called 'Channel_one'
-#     channel_id2 = channels_create_v1(valid_id_1, 'Channel_two', False) # User with valid_id_1 create a Private channel called 'Channel_two'
-#     channel_id3 = channels_create_v1(valid_id_2, 'Channel_three', False) # User with valid_id_2 create a Private channel called 'Channel_three'
-#     channel_id4 = channels_create_v1(valid_id_3, 'Channel_four', True) # User with valid_id_3 create a Public channel called 'Channel_four'
-#     channels_id = [channel_id1, channel_id2, channel_id3, cchannel_id4]
-#     valid_id = [valid_id_1, valid_id_2, valid_id_3]
-#     channel_num = 4
-    
-#     all_return = [channels_id , valid_id , channel_num]
-#     return all_return
-#     clear_v1()
-
 # No id have stored yet
 # Without channels
 def test_valid_id_1():
@@ -72,12 +54,13 @@ def test_return_content(create_user):
     assert channels_listall_v1(valid_id[0]) == channels_listall_v1(valid_id[1]) == channels_listall_v1(valid_id[2])
     clear_v1()
 
-def test_return_content_1(create_user):
-    valid_id = create_user
-    channel_id_1 = channels_create_v1(valid_id[0], "abc", True)
-    channel_id_2 = channels_create_v1(id_2, "zyx", False)
-    assert channels_listall_v1(valid_id[0]) == channels_listall_v1(valid_id[1]) == channels_listall_v1(valid_id[2])
-    clear_v1()
+
+# def test_return_content_1(create_user):
+#     valid_id = create_user
+#     channel_id_1 = channels_create_v1(valid_id[0], "abc", True)
+#     channel_id_2 = channels_create_v1(id_2, "zyx", False)
+#     assert channels_listall_v1(valid_id[0]) == channels_listall_v1(valid_id[1]) == channels_listall_v1(valid_id[2])
+#     clear_v1()
 
 
 

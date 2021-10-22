bf57a63591eed7ff3c26e8e7e934c0c2cd5492d7
Zami <z5358347@ad.unsw.edu.au>
2021-10-23 02:42:03 +1100

    add user functions to server.py

--- src/server.py
+++ src/server.py
@@ -3,9 +3,10 @@ import signal
 from json import dumps
 from flask import Flask, request
 from flask_cors import CORS
-from src.error import InputError
+from src.error import AccessError, InputError
 from src import config
 from src.auth import auth_register_v2, auth_login_v2, check_name_length, check_password_length, check_valid_email, check_duplicate_email
+from src.users import users_all_v1, user_profile_setname_v1, user_profile_v1, user_profile_setemail_v1, user_profile_sethandle_v1, check_alpha_num, check_duplicate_handle, check_duplicate_email, check_handle, check_valid_email, check_name_length, token_check
 from src.error import InputError
 
 def quit_gracefully(*args):
@@ -83,6 +84,87 @@ def auth_login_http():
 
     return dumps(result)
 
+@APP.route('/users/all/v1', methods=['GET'])
+def users_all_http():
+    token = request.args.get('token')
+
+    if token_check(token) == False:
+        raise AccessError(description="Invalid token")
+
+    result = users_all_v1(token)
+    return dumps(result)
+
+@APP.route('/user/profile/v1', methods=['GET'])
+def user_profile_http():
+    token = request.args.get('token')
+    u_id = request.args.get('u_id')
+
+    if token_check(token) == False:
+        raise AccessError(description="Invalid token")
+    
+    result = user_profile_v1(token, u_id)
+    return dumps(result)
+
+@APP.route('/user/profile/setname/v1', methods=['PUT'])
+def user_profile_setname_http():
+    request_data = request.get_json()
+
+    token = request_data['token']
+    name_first = request_data['name_first']
+    name_last = request_data['name_last']
+
+    if check_name_length(name_first) == False:
+        raise InputError(description='Invalid first name')
+    
+    if check_name_length(name_last) == False:
+        raise InputError(description='Invalid last name')
+    
+    if token_check(token) == False:
+        raise AccessError(description="Invalid token")
+    
+    result = user_profile_setname_v1(token, name_first, name_last)
+    return dumps(result)
+
+@APP.route('/user/profile/setemail/v1', methods=['PUT'])
+def user_profile_setemail_http():
+    request_data = request.get_json()
+
+    token = request_data['token']
+    email = request_data['email']
+
+    if token_check(token) == False:
+        raise AccessError(description="Invalid token")
+    
+    if check_duplicate_email == False:
+        raise InputError(description='Duplicate email')
+    
+    if check_valid_email == False:
+        raise InputError(description="Invalid email")
+
+    result = user_profile_setemail_v1(token, email)
+    return dumps(result)
+
+@APP.route('/user/profile/sethandle/v1', methods=['PUT'])
+def user_profile_sethandle_http():
+    request_data = request.get_json()
+
+    token = request_data['token']
+    handle_str = request_data['handle_str']
+
+    if check_handle == False: 
+        raise InputError(description='Invalid handle')
+    
+    if check_duplicate_handle == False:
+        raise InputError(description='Duplicate handle')
+    
+    if check_alpha_num(handle_str) == False:
+        raise InputError(description='Invalid handle length')
+    
+    if token_check(token) == False:
+        raise AccessError(description="Invalid token")
+
+    result = user_profile_sethandle_v1(token, handle_str)
+    return dumps(result)
 
 #### NO NEED TO MODIFY BELOW THIS POINT
 
--- src/users.py
+++ src/users.py
@@ -4,26 +4,63 @@ import jwt
 from src.data_store import data_store
 from src.error import InputError, AccessError
 from src.token_helpers import decode_JWT
+from src.auth import check_name_length, check_valid_email, check_duplicate_email
 
+# HELPER FUNCTIONS 
+def token_check(token):
+    decoded_token = decode_JWT(token)
+    store = data_store.get()
+    
+    found = False 
+    i = 1
+    while i < len(store['emailpw']):
+        user = store['emailpw'][i]
+        # check if session id matches any current session id’s 
+        if decoded_token['session_id'] == user['session_id']:
+            found = True
+
+        i += 1 
+
+    if found == False:
+        return False
+    
+    pass
+
+def check_handle(handle_str):
+    if len(handle_str) < 3 or len(handle_str) > 20:
+        return False
+
+    pass
+
+def check_duplicate_handle(handle_str):
+    store = data_store.get()
+
+    i = 1
+    while i < len(store['users']):
+        user = store['users'][i]
+        if user['handle_str'] == handle_str:
+            return False 
+        i += 1
+    
+    pass 
+
+def check_alpha_num(string):
+    if string.isalnum() == False:
+        return False
+    
+    pass 
+
+
+# USERS FUNCTIONS
 def users_all_v1(token):
     """
     Returns a list of all users and their associated details.
     """
     store = data_store.get()
 
-    # decode the token and check session id 
-    decoded_token = decode_JWT(token)
-
-    found = False 
-    for user in store['emailpw']:
-		# check if session id matches any current session id’s 
-	    if decoded_token['session_id'] == user['session_id']:
-		    found = True 
-
-    if found == False:
-        raise AccessError("Invalid token") 
-    elif found == True: 
-	    return { 'users': store['users'] } 
+    token_check(token)
+     
+    return { 'users': store['users'] } 
 
 def user_profile_v1(token, u_id):
     """
@@ -32,22 +69,20 @@ def user_profile_v1(token, u_id):
 
     store = data_store.get()
     
-    # decode the token and check session id 
-    decoded_token = decode_JWT(token)
-
-    found = False 
-    for user in store['emailpw']:
-		# check if session id matches any current session id’s 
-	    if decoded_token['session_id'] == user['session_id']:
-		    found = True 
-
-    if found == False:
-        raise AccessError("Invalid token")
+    token_check(token)
 
     # search through users to find correct user 
-    for user in store['users']:
+    # for user in store['users']:
+    #     if u_id == user['u_id']:
+    #         return {'user' : user}
+    
+    i = 1
+    while i < len(store['users']):
+        user = store['users'][i]
         if u_id == user['u_id']:
             return {'user' : user}
+
+        i += 1 
 
     # if no match 
     raise InputError("Invalid u_id")
@@ -56,117 +91,83 @@ def user_profile_setname_v1(token, name_first, name_last):
     """
     Update the authorised user's first and last name
     """
-    # input error if length of name_first is not between 1 and 50 characters inclusive
-    if len(name_first) < 1 or len(name_first) > 50:
-        raise InputError("Invalid first name")
-    
-    # input error if length of name_last is not between 1 and 50 characters inclusive
-    if len(name_last) < 1 or len(name_last) > 50:
-        raise InputError("Invalid last name")
-
     store = data_store.get()
 
-    # decode the token and check session id 
-    decoded_token = decode_JWT(token)
+    check_name_length(name_first)
+    check_name_length(name_last)
 
-    found = False 
-    for user in store['emailpw']:
-		# check if session id matches any current session id’s 
-	    if decoded_token['session_id'] == user['session_id']:
-            found = True 
-            current = decoded_token['u_id']
 
-    if found == False:
-        raise AccessError("Invalid token")
-    
-    for user in store['users']:
-        if user['u_id'] == current:
+    token_check(token)
+
+    # for user in store['users']:
+    #     if user['u_id'] == token['u_id']:
+    #         user['name_first'] = name_first
+    #         user['name_last'] = name_last 
+
+    # update users dict 
+    i = 1
+    while i < len(store['users']):
+        user = store['users'][i]
+        if user['u_id'] == token['u_id']:
             user['name_first'] = name_first
-            user['name_last'] = name_last 
- 
-        data_store.set(store)
-        return { }
+            user['name_last'] = name_last
+            data_store.set(store)
+            return { } 
+
+        i += 1 
     
-    # if user does not exist 
+    # if user does not exist
     raise InputError("Invalid user") 
 
 def user_profile_setemail_v1(token, email):
     """
     Update the authorised user's email address 
     """
-
     store = data_store.get()
-    # input error if invalid email
-    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-    if not (re.fullmatch(regex, email)):
-        raise InputError("Invalid email")    
+
+    check_duplicate_email(email)
+    check_valid_email(email)
+
+    token_check(token)
     
-    # input error if duplicate email 
-    for user in store['users']:
-        if user['email'] == email: 
-            raise InputError("Duplicate email")
-
-    # decode the token and check session id 
-    decoded_token = decode_JWT(token)
-
-    found = False 
-    for user in store['emailpw']:
-		# check if session id matches any current session id’s 
-	    if decoded_token['session_id'] == user['session_id']:
-            found = True
-            current = decoded_token['u_id']    
-
-    if found == False:
-        raise AccessError("Invalid token")
-    
-    for user in store['users']:
-        if user['u_id'] == current:
+    # update users dict 
+    i = 1
+    while i < len(store['users']):
+        user = store['users'][i]
+        if user['u_id'] == token['u_id']:
             user['email'] = email
- 
-        data_store.set(store)
-        return { }
+            data_store.set(store)
+            return { } 
+
+        i += 1 
     
-    # if user does not exist 
-    raise InputError("Invalid email")
+    # if user does not exist
+    raise InputError("Invalid user")
 
 def user_profile_sethandle_v1(token, handle_str):
+
+    check_handle(handle_str)
+    check_duplicate_handle(handle_str)
+    check_alpha_num(handle_str)
+    
     store = data_store.get()
+    token_check(token)
 
-    # input error if duplicate handle 
-    for user in store['users']:
-        if user['handle_str'] == handle_str: 
-            raise InputError("Duplicate handle")
-
-    # input error if characters are not all alphanumeric 
-    if handle_str.isalnum() == False:
-        raise InputError("Not all alphanumeric")
-
-    # input error if length of handle_str is not between 3 and 20 characters inclusive
-    if len(handle_str) < 3 or len(handle_str) > 20:
-        raise InputError("Handle must be between 3 and 20 characters inclusive")
-    
-    # decode the token and check session id 
-    decoded_token = decode_JWT(token)
-
-    found = False 
-    for user in store['emailpw']:
-		# check if session id matches any current session id’s 
-	    if decoded_token['session_id'] == user['session_id']:
-            found = True
-            current = decoded_token['u_id']    
-
-    if found == False:
-        raise AccessError("Invalid token")
-    
-    for user in store['users']:
-        if user['u_id'] == current:
+    # update users dict 
+    i = 1
+    while i < len(store['users']):
+        user = store['users'][i]
+        if user['u_id'] == token['u_id']:
             user['handle_str'] = handle_str
- 
-        data_store.set(store)
-        return { }
+            data_store.set(store)
+            return { } 
+
+        i += 1 
     
-    # if handle does not exist 
-    raise InputError("Invalid handle_str")
+    # if user does not exist
+    raise InputError("Invalid user")
+
+    
 
     
 

8bd2a288d03c8e4b3980c2ad2896993937a86b52
Lilian Pok <lilian.pok@gmail.com>
2021-10-23 17:04:07 +1100

    Http testing finalised for channel leave

--- tests/channel_leave_v1_test.py
+++ tests/channel_leave_v1_test.py
@@ -1,67 +1,24 @@
 import pytest
-from src.error import InputError, AccessError
-from src.channel import channel_join_v2, channel_leave_v1
-from src.channels import channels_create_v2
-from src.auth import auth_register_v2
-from src.other import clear_v1
+import requests
+import json
+from src import config
 
-# Creating valid channel and user IDs, 
-# with one public channel and one private channel
-@pytest.fixture
-def valid_3_users():
-    clear_v1()
-    token_1 = auth_register_v1("abc@abc.com", "password", "abc", "def")["token"]
-    token_2 = auth_register_v1("zyx@wvu.com", "password", "zyx", "wvu")["token"]
-    token_3 = auth_register_v1("jkl@wvu.com", "password", "jkl", "mno")["token"]
-    channel_id_1 = channels_create_v2(token_1, "abc", True)['channel_id']
-    channel_join_v2(token_2, channel_id_1)
-    return token_1, token_2, token_3, channel_id_1
-    
+BASE_URL = 'http://127.0.0.1:8080'
+
 # Testing for invalid channel ID
 def test_invalid_channel(valid_3_users):
-    token_1, *_ = valid_3_users
-    with pytest.raises(InputError):
-        channel_leave_v1(token_2, "invalid_channel")
-
-# Testing for valid channel ID
-def test_valid_channel(valid_3_users):
-    token_1, _, _, channel_id_1 = valid_3_users
-    channel_leave_v1(token_2, channel_id_1)
-
-
-# Testing for a case where the user is not a member of the valid channel
-def test_not_a_member(valid_3_users):
-    _, _, token_3, channel_id_1 = valid_3_users
-    with pytest.raises(AccessError):
-        channel_leave_v1(token_3, channel_id_1)
-
-# Testing for a case where the user is a member of the valid channel
-def test_member(valid_3_users):
-    token_1, _, _, channel_id_1 = valid_3_users
-    channel_leave_v1(token_1, channel_id_1)
-
-# Testing cases for wrong input
-def test_empty():
-    with pytest.raises(InputError):
-        channel_leave_v1("", "")
-
-def test_symbols():
-    with pytest.raises(InputError):
-        channel_leave_v1("", "#$(%}(")
-
-def test_integer_token():
-    with pytest.raises(InputError):
-        channel_leave_v1(1, "invalid_channel")
-
-def test_combination():
-    with pytest.raises(InputError):
-        channel_join_v1("", "invalid_channel")
-        channel_join_v1(1, "")
-        channel_join_v1(2, "#$(%}(")
-
-
-
-
-
+    requests.delete(f'{BASE_URL}/clear/v1')
+    # Create two users
+    requests.post(f'{BASE_URL}/auth/register/v2', json={"email": "abc@email.com", "password": "abc", "Firstname": "abc", "Lastname": "def"})
+    requests.post(f'{BASE_URL}/auth/register/v2', json={"email": "def@email.com", "password": "def", "Firstname": "def", "Lastname": "ghi"})
+    # First user logs in, creates a channel 
+    requests.post(f'{BASE_URL}/auth/login/v2', json={"email": "abc@email.com", "password": "abc"})
+    requests.post(f'{BASE_URL}/channels/create/v2', json={"token": "token_one", "name": "channel1", "is_public": True})
+    # Error; second user trying to leave the channel they are not a member of
+    response = requests.post(f'{BASE_URL}/channel/leave/v1', json={"token": "token_two", "channel_id": 1})
+    assert (response.status_code == 400)
+    # Error; invalid channel
+    response = requests.post(f'{BASE_URL}/channel/leave/v1', json={"token": "token_two", "channel_id": "invalid"})
+    assert (response.status_code == 400)
 
 

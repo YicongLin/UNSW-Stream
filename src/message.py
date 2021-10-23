48f39e1e3bac3bf404e655f44cae0423be574e52
Lilian Pok <lilian.pok@gmail.com>
2021-10-23 17:11:03 +1100

    reating message file

---
+++ src/message.py
@@ -0,0 +1,23 @@
+from src.data_store import data_store
+from src.error import InputError
+from src.error import AccessError
+import hashlib
+import jwt
+
+def message_senddm_v1(token, dm_id, message):
+
+
+# function for sending messages
+def message_send_v1(token, channel_id, message):
+
+return {message_id}
+
+# function for editing messages
+def message_edit_v1(token, message_id, message):
+
+return {}
+
+# function for removing messages
+def message_remove_v1(token, message_id):
+
+return {}

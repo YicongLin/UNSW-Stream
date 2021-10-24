087a7d5194890737d11ba9588aa0ec545faf1368
Prasannaa <z5359919@ad.unsw.edu.au>
2021-10-24 14:03:56 +1100

    Fixing characters error

--- tests/message_send_test.py
+++ tests/message_send_test.py
@@ -78,7 +78,7 @@ def test_invalid_message_length(valid):
                     Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. \
                     Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. \
                     Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, \
-                    sit amet adipiscing sem neque sed ipsum. N" 
+                    sit amet adipiscing sem neque sed ipsum." 
     }
     r = requests.post(f'{BASE_URL}/message/send/v1', json = payload)
     assert r.status_code == 400

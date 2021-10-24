d4802fd5028aaec3cd0252485eab3fa7efb7498f
Lilian Pok <lilian.pok@gmail.com>
2021-10-24 13:43:09 +1100

    Editing tests

--- tests/message_senddm_v1_test.py
+++ tests/message_senddm_v1_test.py
@@ -77,7 +77,7 @@ def test_invalid_message_length(valid):
                     Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. \
                     Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. \
                     Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, \
-                    sit amet adipiscing sem neque sed ipsum. N" 
+                    sit amet adipiscing sem neque sed ipsum. No" 
     }
     r = requests.post(f'{BASE_URL}/message/senddm/v1', json = payload)
     assert r.status_code == 400

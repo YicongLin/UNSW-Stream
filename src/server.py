080be0c004c585c94b3b2067a25e320d0abec075
Yicong Lin <z5368375@ad.unsw.edu.au>
2021-10-31 02:25:02 +1100

    fix merge conflict

--- src/server.py
+++ src/server.py
@@ -5,11 +5,11 @@ from flask import Flask, request
 from flask_cors import CORS
 from src.error import AccessError, InputError
 from src import config
-from src.channel import channel_addowner_v1, channel_details_v2, channel_removeowner_v1
-from src.channel import check_valid_channel_id, check_valid_uid, check_member, channel_owners_ids, check_channel_owner_permissions, start_greater_than_total, already_a_member
+from src.channel import channel_addowner_v1, channel_details_v2, channel_removeowner_v1, channel_invite_v2, channel_messages_v2, channel_join_v2, channel_leave_v1
+from src.channel import check_valid_channel_id, check_valid_uid, check_valid_token, check_member, channel_owners_ids, check_channel_owner_permissions, check_global_owner, start_greater_than_total, channel_status, check_channel
 from src.channels import channels_listall_v2, channels_create_v2, channels_list_v2
 from src.dm import dm_details_v1, dm_leave_v1, dm_create_v1, dm_list_v1, dm_remove_v1
-from src.dm import check_valid_dmid, check_valid_dm_token,check_valid_token
+from src.dm import check_valid_dmid, check_valid_dm_token
 from src.auth import auth_register_v2, auth_login_v2, check_name_length, check_password_length, check_valid_email, check_duplicate_email
 from src.message import valid_dm_id, valid_message_length, member, message_senddm_v1
 from src.admin import valid_uid, only_global_owner, not_a_global_owner, is_valid_token, admin_userpermission_change_v1, admin_user_remove_v1

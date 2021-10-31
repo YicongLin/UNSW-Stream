8cf7a2cf6a9392a8245224184816ba1001268cb9
Lilian Pok <lilian.pok@gmail.com>
2021-10-31 22:12:29 +1100

    message.py finalised, pylint fixed

--- src/message.py
+++ src/message.py
@@ -13,14 +13,18 @@ import math
 # ================== HELPERS =====================
 # ================================================
 
-
 # Raises an error if the dm_id is invalid
 def valid_dm_id(dm_id):
+    # obtaining data
     data = data_store.get()
     dm_details = data['dms_details']
+
+    # looping through data store to create a list of valid dm_ids
     dm_ids_list = []
     for i in range(len(dm_details)):
         dm_ids_list.append(dm_details[i]['dm_id'])
+
+    # raising the error
     if int(dm_id) not in dm_ids_list:
         raise InputError(description="Invalid DM")
 
@@ -31,75 +35,110 @@ def valid_message_length(message):
 
 # Raises an error if the authorised user is not a member of the DM
 def check_dm_member(token, dm_id):
+    # obtaining data
     data = data_store.get()
     decoded_token = decode_JWT(token)
     auth_user_id = decoded_token['u_id']
     dm_details = data['dms_details']
+
+    # creating a list of members within the given DM
     dm_members_list = []
     for i in range(len(dm_details)):
         if dm_details[i]['dm_id'] == int(dm_id):
             dm_members = dm_details[i]['members']
             for j in range(len(dm_members)):
                 dm_members_list.append(dm_members[j]['u_id'])
+    
+    # raising the error
     if auth_user_id not in dm_members_list:
         raise AccessError("Not a member of the DM")
 
 # Obtain and return channel/dm ID that the message is in, along with relevant information
 def return_info(message_id):
+    # obtaining data
     data = data_store.get()
     channels = data['channels_details']
     dms = data['dms_details']
 
+    # if the message is in a channel, finding the channel 
+    # and returning the channel ID, 'channel' type, and the ID of the user that sent the message
     for i in range(len(channels)):
         channel_messages = channels[i]['messages']
         for j in range(len(channel_messages)):
-            if int(channel_messages[j]['message_id']) == int(message_id):
-                return channels[i]['channel_id'], 'channel', channels[j]['u_id']
+            if channel_messages[j]['message_id'] == int(message_id):
+                return channels[i]['channel_id'], 'channel', channel_messages[j]['u_id']
+
+    # if the message is in a DM, finding the DM 
+    # and returning the DM ID, 'dm' type, and the ID of the user that sent the message
     for i in range(len(dms)):
         dm_messages = dms[i]['messages']
         for j in range(len(dm_messages)):
             if int(dm_messages[j]['message_id']) == int(message_id):
-                return dms[i]['dm_id'], 'dm', dms[j]['u_id']
+                return dms[i]['dm_id'], 'dm', dm_messages[j]['u_id']
 
-# Raises an error if the message_id is invalid, or
-def valid_message_id(message_id):
-    data = data_store.get()
-    channels = data['channels_details']
-    dms = data['dms_details']
-    valid = False
-
-    for i in range(len(channels)):
-        channel_messages = channels[i]['messages']
-        for j in range(len(channel_messages)):
-            if int(channel_messages[j]['message_id']) == int(message_id):
-                valid = True
-    for i in range(len(dms)):
-        dm_messages = dms[i]['messages']
-        for j in range(len(dm_messages)):
-            if int(dm_messages[j]['message_id']) == int(message_id):
-                valid = True
-    if valid == False:
-        raise InputError("Invalid message ID")
-
-# Returns false if the authorised user doesn't have owner permissions 
-# in the channel/DM that a given message is in
-def owner_permissions(token):
+# Raises an error if the message_id is invalid in the channel/DM that the authorised user has joined
+def valid_message_id(token, message_id):
+    # obtaining data
     data = data_store.get()
     decoded_token = decode_JWT(token)
     auth_user_id = decoded_token['u_id']
     channels = data['channels_details']
     dms = data['dms_details']
 
-    a, b, c = return_info(message_id)
-    owner_members_list1 = []
+    # looping through all channels to determine which ones the authorised user is a member of
+    valid = False
+    for i in range(len(channels)):
+        channel_members = channels[i]['channel_members']
+        for j in range(len(channel_members)):
+            if channel_members[j]['u_id'] == auth_user_id:
+                channel_messages = channels[i]['messages']
+                # looping through the messages in the channel to find a match for the message_id
+                for j in range(len(channel_messages)):
+                    # the message_id is valid if there is a match
+                    if channel_messages[j]['message_id'] == int(message_id):
+                        valid = True
+
+    # looping through all DMs to determine which ones the authorised user is a member of
+    for i in range(len(dms)):
+        dm_members = dms[i]['members']
+        for j in range(len(dm_members)):
+            if dm_members[j]['u_id'] == auth_user_id:
+                dm_messages = dms[i]['messages']
+                # looping through the messages in the DM to find a match for the message_id
+                for j in range(len(dm_messages)):
+                    # the message_id is valid if there is a match
+                    if dm_messages[j]['message_id'] == int(message_id):
+                        valid = True
+    
+    # raising the error if there is no match
+    if valid == False:
+        raise InputError("Invalid message ID")
+
+# Returning false if the authorised user doesn't have owner permissions 
+# in the channel/DM that a given message is in
+def owner_permissions(token, message_id):
+    # obtaining data
+    data = data_store.get()
+    decoded_token = decode_JWT(token)
+    auth_user_id = decoded_token['u_id']
+    channels = data['channels_details']
+    dms = data['dms_details']
+
+    # determining what channel/DM the message is in and obtaining information
+    a, b, _ = return_info(message_id)
+
+    # if the message is in a channel, creating a list of channel owner members
+    owner_members_list = []
     if b == 'channel':
         for i in range(len(channels)):
             if channels[i]['channel_id'] == int(a):
                 owner_members = channels[i]['owner_members']
                 for j in range(len(owner_members)):
                     owner_members_list.append(owner_members[j]['u_id'])
-    if  auth_user_id not in owner_members_list:
-        return False
+        if auth_user_id not in owner_members_list:
+            return False
+
+    # if the message is in a DM, determing whether the authorised user is the creator of the DM
     if b == 'dm':
         for i in range(len(dms)):
             if dms[i]['dm_id'] == int(a):
@@ -111,18 +150,20 @@ def owner_permissions(token):
 # - the message was sent by the authorised user
 # - the authorised user has owner permisisons in the channel/DM
 def conditional_edit(token, message_id):
-    data = data_store.get()
+    # obtaining data
     decoded_token = decode_JWT(token)
     auth_user_id = decoded_token['u_id']
 
-    a, b, c = return_info(message_id)
-    if int(c) != auth_user_id and owner_permissions(token) == False:
+    # determining what channel/DM the message is in and obtaining information
+    _, _, c = return_info(message_id)
+
+    # raising the error 
+    if int(c) != auth_user_id and owner_permissions(token, message_id) == False:
         raise AccessError(description="You do not have access to edit the message")
 
 # ================================================
 # ================= FUNCTIONS ====================
 # ================================================
-
 
 def message_senddm_v1(token, dm_id, message):
     """Send a message from the authorised user to the specified DM.
@@ -133,63 +174,57 @@ def message_senddm_v1(token, dm_id, message):
         message (string) - message to be sent
        
     Exceptions:
-        InputError - Occurs when dm_id does not refer to a valid channel
+        InputError - Occurs when dm_id does not refer to a valid DM
         InputError - Occurs when the message length is invalid
         AccessError - Occurs when the authorised user is not a member of the valid DM
     
     Return Value:
         Returns message_id on all valid conditions
     """
-    #Obtaining data
+    # obtaining data
     data = data_store.get()
     decoded_token = decode_JWT(token)
     auth_user_id = decoded_token['u_id']
-
-    # Check for invalid token
-    token_check(token)    
-
-    # Check for valid dm_id
-    valid_dm_id(dm_id) 
-    
-    # Check for valid message length
-    valid_message_length(message)
-    
-    # Raise an error if the authorised user is not a member of the DM
-    check_dm_member(token, dm_id)
-    
-    # Otherwise, send the message to the specified DM
-
-    # Obtain the total number of messages existing in order to assign a message ID
-    length_of_messages = 0
     dm_details = data['dms_details']
     channel_details = data['channels_details']
+
+    # checks for exceptions
+    token_check(token)    
+    valid_dm_id(dm_id) 
+    valid_message_length(message)
+    check_dm_member(token, dm_id)
+    
+    # otherwise, send the message to the specified DM
+
+    # obtain the total number of messages existing in order to assign a message ID
+    length_of_messages = 0
     for i in range(len(dm_details)):
         length_of_messages += len(dm_details[i]['messages'])
     for i in range(len(channel_details)):
         length_of_messages += len(channel_details[i]['messages'])
  
-    # Assigning the message ID
+    # assigning the message ID
     message_id = length_of_messages + 1
 
-    # Obtaining time created
+    # obtaining the time the message is created
     time = datetime.now()
-    time_created = time.replace(tzinfo=timezone.utc).timestamp()
+    time_created = math.floor(time.replace(tzinfo=timezone.utc).timestamp())
 
-    # Creating a dictionary with the message and corresponding information
+    # creating a dictionary with the message and corresponding information
     message_dict = {
         'message_id': message_id,
         'u_id': auth_user_id,
         'message': message,
         'time_created': time_created,
     }
-    # Finding the DM with given dm_id
+
+    # finding the DM with given dm_id and appending the message to the DM's details
     for i in range(len(dm_details)):
-        if dm_details[i]['dm_id'] == dm_id:
+        if dm_details[i]['dm_id'] == int(dm_id):
             data['dms_details'][i]['messages'].append(message_dict)
 
     return {"message_id": message_id}
 
-# function for sending messages
 def message_send_v1(token, channel_id, message):
     """
      Send a message from the authorised user to the specified channel.
@@ -208,53 +243,49 @@ def message_send_v1(token, channel_id, message):
         Returns message_id on all valid conditions
     """
  
-    # Obtaining data
+    # obtaining data
     data = data_store.get()
     decoded_token = decode_JWT(token)
     auth_user_id = decoded_token['u_id']
-
-    # Check for valid channel_id
-    check_channel(channel_id)
-    
-    # Check for valid message length
-    valid_message_length(message)
-
-    # Raise an error if the authorised user is not a member of the channel
-    not_a_member(auth_user_id, channel_id)
-    
-    # Otherwise, send the message to the specified channel
-
-    # Obtain the total number of messages existing in order to assign a message ID
-    length_of_messages = 0
     dm_details = data['dms_details']
     channel_details = data['channels_details']
+
+    # checks for exceptions
+    check_channel(channel_id)
+    valid_message_length(message)
+    not_a_member(auth_user_id, channel_id)
+    
+    # otherwise, send the message to the specified channel
+
+    # obtain the total number of messages existing in order to assign a message ID
+    length_of_messages = 0
     for i in range(len(dm_details)):
         length_of_messages += len(dm_details[i]['messages'])
     for i in range(len(channel_details)):
         length_of_messages += len(channel_details[i]['messages'])
  
-    # Assigning the message ID
+    # assigning the message ID
     message_id = length_of_messages + 1
 
-    # Obtaining time created
+    # obtaining the time the message is created
     time = datetime.now()
     time_created = math.floor(time.replace(tzinfo=timezone.utc).timestamp())
 
-    # Creating a dictionary with the message and corresponding information
+    # creating a dictionary with the message and corresponding information
     message_dict = {
         'message_id': message_id,
         'u_id': auth_user_id,
         'message': message,
         'time_created': time_created
     }
-    # Finding the channel with given channel_id
+
+    # finding the channel with given channel_id and appending the message to the DM's details
     for i in range(len(channel_details)):
         if int(channel_details[i]['channel_id']) == int(channel_id):
             data['channels_details'][i]['messages'].append(message_dict)
     
     return {"message_id": message_id}
 
-# Function for editing messages
 def message_edit_v1(token, message_id, message):
     """
     Given a message, update its text with new text.
@@ -273,35 +304,55 @@ def message_edit_v1(token, message_id, message):
     Return Value:
         Returns an empty dictionary on all valid conditions. 
     """
-    # Obtaining data
+    # obtaining data
     data = data_store.get()
-    decoded_token = decode_JWT(token)
-    auth_user_id = decoded_token['u_id']
+    dm_details = data['dms_details']
+    channel_details = data['channels_details']
 
-    # Checking that length of messages are over 1 and under 1000 characters 
+    # obtaining what channel/DM the message is in 
+    a, b, _ = return_info(message_id)
+
+    # checks for exceptions
     valid_message_length(message)
-
-    # Checking that message_id refers to a valid message
-    valid_message_id(message_id)
-
-    # Checking that the message was sent by the authorised user making the request 
-    # and that the authorised user has owner permissions 
+    valid_message_id(token, message_id)
     conditional_edit(token, message_id)
 
-# Editing the message in a dm
-    dm_details = data['dms_details']
-    for i in range(len(dm_details)):
-        length_of_messages += len(dm_details[i]['messages'])
-    unedited_messages = data[dm_details]['messages']
-    unedited_messages['message'] = edited_message
+    # if the message is in a DM, access the DM the message is in, in order to edit the message
+    if b == 'dm':
+        for i in range(len(dm_details)):
+            if dm_details[i]['dm_id'] == int(a):
+                dm_messages = dm_details[i]['messages']
+                for j in range(len(dm_messages)):
+                    # find the message
+                    if dm_messages[j]['message_id'] == int(message_id):
+                        # if the new message is an empty string, delete the message
+                        if message == "":
+                            dm_messages.remove(dm_messages[j])
+                            data_store.set(data)
+                            break
+                        # otherwise, replace the message with the new text
+                        else:
+                            dm_messages[j]['message'] = message
+                            data_store.set(data)
 
-# Editing the message in a channel
-    channel_details = data['channels_details']
-    for i in range(len(dm_details)):
-        length_of_messages += len(channel_details[i]['messages'])
-    undeleted_channel_messages = data[channel_details]['messages']
-    undeleted_channel_messages['message'].remove(message)  
-    
+    # if the message is in a channel, access the channel the message is in, in order to edit the message
+    elif b == 'channel':
+        for i in range(len(channel_details)):
+            if channel_details[i]['channel_id'] == int(a):
+                channel_messages = channel_details[i]['messages']
+                for j in range(len(channel_messages)):
+                    # find the message
+                    if channel_messages[j]['message_id'] == int(message_id):
+                        # if the new message is an empty string, delete the message
+                        if message == "":
+                            channel_messages.remove(channel_messages[j])
+                            data_store.set(data)
+                            break
+                        # otherwise, replace the message with the new text
+                        else:
+                            channel_messages[j]['message'] = message
+                            data_store.set(data)
+
     return {}
 
 # function for removing messages
@@ -322,30 +373,41 @@ def message_remove_v1(token, message_id):
     Return Value:
         Returns an empty dictionary on all valid conditions. 
     """
-# Obtaining data
+    # obtaining data
     data = data_store.get()
-    decoded_token = decode_JWT(token)
-    auth_user_id = decoded_token['u_id']
+    dm_details = data['dms_details']
+    channel_details = data['channels_details']
 
-# Checking that message_id refers to a valid message
-    valid_message_id(message_id)
+    # obtaining what channel/DM the message is in 
+    a, b, _ = return_info(message_id)
 
-# Checking that the message was sent by the authorised user making the request 
-# and that the authorised user has owner permissions 
+    # checks for exceptions
+    valid_message_id(token, message_id)
     conditional_edit(token, message_id)
 
-# Removing the message from DMS
-    dm_details = data['dms_details']
-    for i in range(len(dm_details)):
-        length_of_messages += len(dm_details[i]['messages'])
-    undeleted_dm_messages = data[dm_details]['messages']
-    undeleted_dm_messages['message'].remove(message)  
+    # if the message is in a DM, access the DM the message is in, in order to remove the message
+    if b == 'dm':
+        for i in range(len(dm_details)):
+            if dm_details[i]['dm_id'] == int(a):
+                dm_messages = dm_details[i]['messages']
+                for j in range(len(dm_messages)):
+                    # find the message
+                    if dm_messages[j]['message_id'] == int(message_id):
+                        dm_messages.remove(dm_messages[j])
+                        data_store.set(data)
+                        break
 
-#Removing the message from channels
-    channel_details = data['channels_details']
-    for i in range(len(channel_details)):
-        length_of_messages += len(channel_details[i]['messages'])
-    undeleted_channel_messages = data[channel_details]['messages']
-    undeleted_channel_messages['message'].remove(message)  
-
+    # if the message is in a channel, access the channel the message is in, in order to edit the message
+    elif b == 'channel':
+        for i in range(len(channel_details)):
+            if channel_details[i]['channel_id'] == int(a):
+                channel_messages = channel_details[i]['messages']
+                for j in range(len(channel_messages)):
+                    # find the message
+                    if channel_messages[j]['message_id'] == int(message_id):
+                        channel_messages.remove(channel_messages[j])
+                        data_store.set(data)
+                        break
+                       
     return {}
+    

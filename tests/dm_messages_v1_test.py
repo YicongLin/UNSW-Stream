ce6df3fce9fe40bbee44b25f884f12baccf5e308
Prasannaa <z5359919@ad.unsw.edu.au>
2021-11-02 02:05:27 +1100

    Adding changes to tests

---
+++ tests/dm_messages_v1_test.py
@@ -0,0 +1,167 @@
+import pytest
+import requests
+import json
+from src import config
+import math
+from datetime import datetime, timezone
+
+BASE_URL = 'http://127.0.0.1:3178'
+
+# ================================================
+# ================= FIXTURES =====================
+# ================================================
+
+# Clear data store
+@pytest.fixture
+def setup_clear():
+    requests.delete(f'{BASE_URL}/clear/v1')
+
+# Register first user
+@pytest.fixture
+def registered_first():
+    payload = {
+        "email": "first@email.com", 
+        "password": "password", 
+        "name_first": "first", 
+        "name_last": "user"
+        }
+    r = requests.post(f'{BASE_URL}/auth/register/v2', json = payload)
+    resp = r.json()
+    return resp
+
+# Register second user
+@pytest.fixture
+def registered_second():
+    payload = {
+        "email": "second@email.com", 
+        "password": "password", 
+        "name_first": "second", 
+        "name_last": "user"
+        }
+    r = requests.post(f'{BASE_URL}/auth/register/v2', json = payload)
+    resp = r.json()
+    return resp
+
+# First user creates a public dm
+@pytest.fixture
+def dm_one(registered_first):
+    token = registered_first['token']
+    payload = {
+        "token": token,
+        "name": "1",
+        "is_public": True
+    }
+    r = requests.post(f'{BASE_URL}/dm/create/v1', json = payload)
+    resp = r.json()
+    return resp
+
+# Second user creates a private dm
+@pytest.fixture
+def dm_two(registered_second):
+    token = registered_second['token']
+    payload = {
+        "token": token,
+        "name": "2",
+        "is_public": False
+    }
+    r = requests.post(f'{BASE_URL}/dm/create/v1', json = payload)
+    resp = r.json()
+    return resp
+
+# ================================================
+# =================== TESTS ======================
+# ================================================
+
+# Testing for invalid dm ID
+def test_invalid_dm(setup_clear, registered_first):
+    # first user registers; obtain token
+    token = register_first['token']
+    # first user request messages of a dm with invalid ID 
+    payload2 = {
+        "token": token,
+        "dm_id": 5,
+        "start": 0
+    }
+    r = requests.get(f'{BASE_URL}/dm/messages/v1', params = payload2)
+    assert r.status_code == 400
+
+# Testing for a case where start is greater 
+# than the total number of messages in the dm
+def test_start_greater(setup_clear, registered_first, dm_one):
+    # first user registers; obtain token
+    token = registered_first['token']
+    # first user creates dm; obtain dm_id
+    dm_id = dm_one['dm_id']
+    # first user requests dm messages with an invalid start number
+    payload2 = {
+        "token": token,
+        "dm_id": dm_id,
+        "start": 2
+    }
+    r = requests.get(f'{BASE_URL}/dm/messages/v1', params = payload2)
+    assert r.status_code == 400
+
+# Testing for invalid token ID
+def test_invalid_token(setup_clear, registered_first, dm_one):
+    # first user registers; obtain token
+    token = registered_first['token']
+    # first user creates dms; obtain dm_id
+    dm_id = dm_one['dm_id']
+    # first user logs out; this invalidates the token
+    requests.post(f'{BASE_URL}/auth/logout/v1', json = {"token": token})
+    # first user attempts to request channel messages
+    payload = {
+        "token": token,
+        "dm_id": int(dm_id),
+        "start": 0
+    }
+    r = requests.get(f'{BASE_URL}/dm/messages/v1', params = payload)
+    assert r.status_code == 403
+
+# Testing for a case where the user is not a member of the dm
+def test_not_a_member(setup_clear, registered_first, dm_two):
+    # first user registers; obtain token
+    token = registered_first['token']
+    # second user creates dm; obtain dm_id
+    dm_id = dm_two['dm_id']
+    # first user attempts to request dm messages
+    payload = {
+        "token": token,
+        "dm_id": int(dm_id),
+        "start": 0
+    }
+    r = requests.get(f'{BASE_URL}/dm/messages/v1', params = payload)
+    assert r.status_code == 403
+
+def test_valid(setup_clear, registered_first, dm_one):
+    # first user registers; obtain token and u_id
+    token = registered_first['token']
+    u_id = registeedr_first['auth_user_id']
+    # first user creates dm; obtain dm_id
+    dm_id = dm_one['dm_id']
+    # first user sends message to dm
+    payload1 = {
+        "token": token,
+        "dm_id": int(dm_id),
+        "message": "Goodnight"
+    }
+    requests.post(f'{BASE_URL}/message/send/v1', json = payload1)
+    # obtaining the time the message is created
+    time = datetime.now()
+    time_created = math.floor(time.replace(tzinfo=timezone.utc).timestamp())
+    # first user requests dm messages
+    payload2 = {
+        "token": token,
+        "dm_id": int(dm_id),
+        "start": 0
+    }
+    r = requests.get(f'{BASE_URL}/dm/messages/v1', params = payload2)
+    assert r.status_code == 200
+    message = {
+        "message_id": 1,
+        "u_id": u_id,
+        "message": "Goodnight",
+        "time_created": time_created
+    }
+    response = r.json()
+    assert response == {"messages": [message], "start": 0, "end": -1}

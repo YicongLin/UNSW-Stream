cec66cb0470be71f081a6e7f8e8043f791e4476c
Prasannaa <z5359919@ad.unsw.edu.au>
2021-11-13 02:52:25 +1100

    Fixing pylint for tests

--- tests/message_unpin_v1_test.py
+++ tests/message_unpin_v1_test.py
@@ -4,8 +4,9 @@ import json
 from src import config
 import math
 from datetime import datetime, timezone
+from src.config import url
 
-BASE_URL = 'http://127.0.0.1:2000'
+BASE_URL = url
 
 # ================================================
 # ================= FIXTURES =====================
@@ -104,20 +105,15 @@ def test_invalid_message_id(setup_clear, registered_second, channel_two):
     token = registered_second['token']
     # second user creates channel; obtain channel_id
     channel_id = channel_two['channel_id']
-    # second user attempts to edit a message with an invalid message_id
+    # second user attempts to unpin a message with an invalid message_id
     payload = {
         "token": token,
         "channel_id": channel_id,
         "message_id": 1,
-        "message": "Hello World"
+        "message": "Hello World",
+        "is_pinned": True
     }
     r = requests.post(f'{BASE_URL}/message/unpin/v1', json = payload)
-    assert r.status_code == 400 
-
-# Testing for when the message is not pinned
-def test_message_unpinned()
-
-    r = requests.post(f'{BASE_URL}/message/pin/v1', json = payload)
     assert r.status_code == 400 
 
 # Testing for a case where the authorised user did not send the original message,
@@ -133,7 +129,8 @@ def test_no_user_permission_channel(setup_clear, registered_first, registered_se
     payload = {
         "token": token_2,
         "dm_id": dm_id,
-        "message": "Hi"
+        "message": "Hi",
+        "is_pinned": True
     }
     requests.post(f'{BASE_URL}/message/senddm/v1', json = payload)
     # second user creates channel; obtain channel_id
@@ -148,16 +145,18 @@ def test_no_user_permission_channel(setup_clear, registered_first, registered_se
     payload2 = {
         "token": token_2,
         "channel_id": channel_id,
-        "message": "Hi"
+        "message": "Hi",
+        "is_pinned": True
     }
     requests.post(f'{BASE_URL}/message/send/v1', json = payload2)
-    # first user attempts to edit the message
+    # first user attempts to unpin the message
     payload3 = {
         "token": token_1,
         "message_id": 2,
-        "message": "Bye"
+        "message": "Hi",
+        "is_pinned": False
     }
-    r = requests.put(f'{BASE_URL}/message/edit/v1', json = payload3)
+    r = requests.put(f'{BASE_URL}/message/unpin/v1', json = payload3)
     assert r.status_code == 403
 
 # Testing for a valid case where the authorised user has owner permissions in the DM
@@ -175,16 +174,18 @@ def test_owner_permission_dm(setup_clear, registered_first, registered_second, d
     payload1 = {
         "token": token_1,
         "dm_id": dm_id,
-        "message": "Hi"
+        "message": "Hi",
+        "is_pinned": True
     }
     requests.post(f'{BASE_URL}/message/senddm/v1', json = payload1)
-    # second user edits the message
+    # second user unpins the message
     payload2 = {
         "token": token_2,
         "message_id": 1,
-        "message": "Bye"
+        "message": "Hi",
+        "is_pinned": False
     }
-    r = requests.put(f'{BASE_URL}/message/edit/v1', json = payload2)
+    r = requests.put(f'{BASE_URL}/message/unpin/v1', json = payload2)
     assert r.status_code == 200 
 
 # Testing for a valid case where the authorised user has owner permissions in the channel
@@ -208,19 +209,21 @@ def test_owner_permission_channel(setup_clear, registered_first, registered_seco
     payload1 = {
         "token": token_1,
         "channel_id": channel_id,
-        "message": "Hi"
+        "message": "Hi",
+        "is_pinned": True
     }
     requests.post(f'{BASE_URL}/message/send/v1', json = payload1)
-    # second user edits the message
+    # second user unpins the message
     payload2 = {
         "token": token_2,
         "message_id": 1,
-        "message": "Bye"
+        "message": "Hi",
+        "is_pinned": False
     }
-    r = requests.put(f'{BASE_URL}/message/edit/v1', json = payload2)
+    r = requests.put(f'{BASE_URL}/message/unpin/v1', json = payload2)
     assert r.status_code == 200 
 
-# Testing for a valid case where the authorised user sent the message
+# Testing for an invalid case where the authorised user sent the message
 # but isn't an owner of the channel
 def test_not_owner_valid(setup_clear, registered_first, channel_two):
     # first user registers; obtain token
@@ -237,26 +240,141 @@ def test_not_owner_valid(setup_clear, registered_first, channel_two):
     payload2 = {
         "token": token,
         "channel_id": channel_id,
-        "message": "Hi"
+        "message": "Hi",
+        "is_pinned": True
+
     }
     requests.post(f'{BASE_URL}/message/send/v1', json = payload2)
-    # first user edits the message
+    # first user unpins the message
     payload3 = {
         "token": token,
         "message_id": 1,
-        "message": "Bye"
+        "message": "Hi",
+        "is_pinned": False
     }
-    r = requests.put(f'{BASE_URL}/message/edit/v1', json = payload3)
+    r = requests.put(f'{BASE_URL}/message/unpin/v1', json = payload3)
+    assert r.status_code == 403
+
+# Testing for when the message is already unpinned in the channel
+def test_message_already_unpinned_channel(setup_clear, registered_first, registered_second, channel_one, channel_two):
+    # first user registers; obtain token
+    token_1 = registered_first['token']
+    # second user registers; obtain token
+    token_2 = registered_second['token']
+    # first user creates channel
+    channel_one
+    # second user creates channel; obtain channel_id
+    channel_id = channel_two['channel_id']
+    # first user joins second channel
+    payload = {
+        "token": token_1,
+        "channel_id": channel_id
+    }
+    requests.post(f'{BASE_URL}/channel/join/v2', json = payload)
+    # first user sends a message to the channel
+    payload1 = {
+        "token": token_1,
+        "channel_id": channel_id,
+        "message": "Hi",
+        "is_pinned": False
+    }
+    requests.post(f'{BASE_URL}/message/send/v1', json = payload1)
+    # second user attempts to unpin the message
+    payload2 = {
+        "token": token_2,
+        "message_id": 1,
+        "message": "Hi",
+        "is_pinned": False
+    }
+    r = requests.put(f'{BASE_URL}/message/unpin/v1', json = payload2)
+    assert r.status_code == 400
+
+# Testing for when the message is already unpinned in the DM
+def test_already_unpinned_dm(setup_clear, registered_first, registered_second, dm_one, dm_two):
+    # first user registers; obtain token
+    token_1 = registered_first['token']
+    # second user registers; obtain token
+    token_2 = registered_second['token']
+    # first user creates DM
+    dm_one
+    # second user creates DM with first user; obtain dm_id
+    dm_id = dm_two['dm_id']
+    # first user sends a message to the DM
+    payload1 = {
+        "token": token_1,
+        "dm_id": dm_id,
+        "message": "Hi",
+        "is_pinned": False
+    }
+    requests.post(f'{BASE_URL}/message/senddm/v1', json = payload1)
+    # second user attempts to unpin the message
+    payload2 = {
+        "token": token_2,
+        "message_id": 1,
+        "message": "Hi",
+        "is_pinned": False
+    }
+    r = requests.put(f'{BASE_URL}/message/unpin/v1', json = payload2)
+    assert r.status_code == 400
+
+# Testing for when there is a successful unpin in a channel
+def test_successful_unpin_channel(setup_clear, registered_first, registered_second, channel_one, channel_two):
+    # first user registers; obtain token
+    token_1 = registered_first['token']
+    # second user registers; obtain token
+    token_2 = registered_second['token']
+    # first user creates channel
+    channel_one
+    # second user creates channel; obtain channel_id
+    channel_id = channel_two['channel_id']
+    # first user joins second channel
+    payload = {
+        "token": token_1,
+        "channel_id": channel_id
+    }
+    requests.post(f'{BASE_URL}/channel/join/v2', json = payload)
+    # second user sends a message to the channel
+    payload1 = {
+        "token": token_2,
+        "channel_id": channel_id,
+        "message": "Hi",
+        "is_pinned": True
+    }
+    requests.post(f'{BASE_URL}/message/send/v1', json = payload1)
+    # second user pins the message
+    payload2 = {
+        "token": token_2,
+        "message_id": 1,
+        "message": "Hi",
+        "is_pinned": False
+    }
+    r = requests.post(f'{BASE_URL}/message/unpin/v1', json = payload2)
+    assert r.status_code == 200
+
+# Testing for when there is a successful unpin in a DM
+def test_successful_pin_dm(setup_clear, registered_first, registered_second, dm_one, dm_two):
+    # first user registers; obtain token
+    token_1 = registered_first['token']
+    # second user registers; obtain token
+    token_2 = registered_second['token']
+    # first user creates DM
+    dm_one
+    # second user creates DM with first user; obtain dm_id
+    dm_id = dm_two['dm_id']
+    # second user sends a message to the DM
+    payload1 = {
+        "token": token_1,
+        "dm_id": dm_id,
+        "message": "Hi",
+        "is_pinned": True
+    }
+    requests.post(f'{BASE_URL}/message/senddm/v1', json = payload1)
+    # second user unpins the message
+    payload2 = {
+        "token": token_2,
+        "message_id": 1,
+        "message": "Hi",
+        "is_pinned": False
+    }
+    r = requests.put(f'{BASE_URL}/message/unpin/v1', json = payload2)
     assert r.status_code == 200 
-
-# Testing for when there is a successful unpin in a channel
-def test_successful_unpin_channel()
-
-    r = requests.post(f'{BASE_URL}/message/react/v1', json = payload)
-    assert r.status_code == 200
-
-# Testing for when there is a successful unpin in a channel
-def test_successful_unpin_dm()
-
-    r = requests.post(f'{BASE_URL}/message/react/v1', json = payload)
-    assert r.status_code == 200

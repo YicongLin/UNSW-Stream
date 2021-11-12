333cb54f1c30c35e79c27490f9d2230a3a18d377
Zami <z5358347@ad.unsw.edu.au>
2021-11-13 02:18:01 +1100

    comment out user_profile_uploadphoto

--- src/users.py
+++ src/users.py
@@ -229,52 +229,52 @@ def user_profile_sethandle_v1(token, handle_str):
 
 
 
-def user_profile_uploadphoto_v1(token, img_url, x_start, y_start, x_end, y_end):
-    data = data_store.get()
+# def user_profile_uploadphoto_v1(token, img_url, x_start, y_start, x_end, y_end):
+#     data = data_store.get()
     
-    # Raise an AccessError if authorised user login with an invalid token
-    token_check(token)
+#     # Raise an AccessError if authorised user login with an invalid token
+#     token_check(token)
 
-    auth_user_id = decode_JWT(token)['u_id']
+#     auth_user_id = decode_JWT(token)['u_id']
 
-    # Raise an InputError if authorised user pass in unavailable image url
+#     # Raise an InputError if authorised user pass in unavailable image url
     
 
-    try:
-        # Get and store the image user upload
-        urllib.request.urlretrieve(img_url, f"src/static/{auth_user_id}_temp.jpg")
-        img = Image.open(f"src/static/{auth_user_id}_temp.jpg")
+#     try:
+#         # Get and store the image user upload
+#         urllib.request.urlretrieve(img_url, f"src/static/{auth_user_id}_temp.jpg")
+#         img = Image.open(f"src/static/{auth_user_id}_temp.jpg")
 
-        # Raise an InputError if authorised user pass in not JPG image
-        if img.format != "JPG" and img.format != "JPEG":
-            raise InputError(description="Unacceptable image format")
+#         # Raise an InputError if authorised user pass in not JPG image
+#         if img.format != "JPG" and img.format != "JPEG":
+#             raise InputError(description="Unacceptable image format")
 
-        # Obtain the size of image
-        width, height = img.size
+#         # Obtain the size of image
+#         width, height = img.size
 
-        # Raise an InputError if authorised user type in a bound out of dimensions of the image
-        if x_start not in range (0, width + 1) or x_end not in range (0, width + 1):
-            raise InputError(description="Out of dimensions of the image")
+#         # Raise an InputError if authorised user type in a bound out of dimensions of the image
+#         if x_start not in range (0, width + 1) or x_end not in range (0, width + 1):
+#             raise InputError(description="Out of dimensions of the image")
 
-        if y_start not in range (0, height + 1) or y_end not in range (0, height + 1):
-            raise InputError(description="Out of dimensions of the image")
+#         if y_start not in range (0, height + 1) or y_end not in range (0, height + 1):
+#             raise InputError(description="Out of dimensions of the image")
 
-        # Raise an InputError if authorised user type in a impossiable crop bound 
-        if x_end < x_start or y_end < y_start:
-            raise InputError(description="Impossible crop image bounds")
+#         # Raise an InputError if authorised user type in a impossiable crop bound 
+#         if x_end < x_start or y_end < y_start:
+#             raise InputError(description="Impossible crop image bounds")
 
-        # Crop image and save it
-        cropped = img.crop((x_start, y_start, x_end, y_end))
-        cropped.save(f"src/static/{auth_user_id}.jpg")
+#         # Crop image and save it
+#         cropped = img.crop((x_start, y_start, x_end, y_end))
+#         cropped.save(f"src/static/{auth_user_id}.jpg")
 
-        # Obtain index of authorised user's user dict store in users list
-        # Update image url to authorised user's user dict
-        user_index = users_index(auth_user_id)
-        # data['users'][user_index]['profile_img_url'] = url_for('static', filename=f'{auth_user_id}.jpg', _external=True)
+#         # Obtain index of authorised user's user dict store in users list
+#         # Update image url to authorised user's user dict
+#         user_index = users_index(auth_user_id)
+#         data['users'][user_index]['profile_img_url'] = url_for('static', filename=f'{auth_user_id}.jpg', _external=True)
 
-    except (HTTPError, URLError):
-        # if urllib.request.urlopen(img_url).status != 200 or urllib.request.urlretrieve.status != 200:
-        raise InputError(description="Unavailable image url") from None
+#     except (HTTPError, URLError):
+#         # if urllib.request.urlopen(img_url).status != 200 or urllib.request.urlretrieve.status != 200:
+#         raise InputError(description="Unavailable image url") from None
 
-    return {}
+#     return {}
 
